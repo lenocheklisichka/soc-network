96a93787937d6c9d8bcac57bab006aa77fd09a20
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2021-10-01 18:06:02 +0300

    refactor index

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,18 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from "react-dom";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import store from "./redux/redux-store";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <React.StrictMode>
                 <App/>
             </React.StrictMode>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
